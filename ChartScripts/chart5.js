bfd9319ab8ad14fae2191939af1a788c91d48014
Brian Sweeney <bsweeney930@gmail.com>
2017-05-15 23:37:41 -0400

    all exhibits (less 2) responsive

--- ChartScripts/chart5.js
+++ ChartScripts/chart5.js
@@ -1,5 +1,5 @@
-var w = 900;
-var h = 550;
+var w = window.outerWidth - 50;
+var h = w * .625 - 50;
 var marginFive = {
   top: 98,
   bottom: 60,
@@ -329,7 +329,7 @@ function resizeFive(params){
       }
   }
 
-  d3.select(this.node().parentNode)//resize svg element
+  d3.select(chartFive.node().parentNode)//resize svg element
       .attr('height', h)
       .attr('width', w)
 
